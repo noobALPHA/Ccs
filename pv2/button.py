04975b3272142228fad631986a531198c5492996
0×ALPHA <68905945+noobALPHA@users.noreply.github.com>
2023-06-27 23:43:53 +0530

    Update button.py

--- pv2/button.py
+++ pv2/button.py
@@ -1,7 +1,7 @@
 from pyrogram.types import InlineKeyboardButton
 
 def add_button(message):
-    button = InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=12345686)
+    button = InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=1057412250)
     if message.reply_markup:
         message.reply_markup.inline_keyboard.append([button])
     else:
